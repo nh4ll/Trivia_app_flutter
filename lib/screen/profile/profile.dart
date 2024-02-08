a79f486435984ce74877dfb768568e3ac8ccfe1c
muhdsabith <sabith4400@gmail.com>
2024-02-08 10:31:44 +0530

    profile page color changed

--- lib/screen/profile/profile.dart
+++ lib/screen/profile/profile.dart
@@ -12,7 +12,7 @@ class Profile extends StatelessWidget {
             height: 150,
             width: 400,
             decoration: BoxDecoration(
-              color: Colors.white,
+              color: Colors.black,
               borderRadius:BorderRadius.only(
                 bottomLeft: Radius.circular(30),
                 bottomRight: Radius.circular(30),
@@ -36,7 +36,7 @@ class Profile extends StatelessWidget {
             ),
           Padding(padding: EdgeInsets.only(top: 200,left: 160),
           child:Text('sabith',style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
             fontSize: 24
           ),) ,
           ),
@@ -45,7 +45,7 @@ class Profile extends StatelessWidget {
             children: [
               Padding(padding: EdgeInsets.only(top: 230,left: 165),
               child:Text('@sabith',style: TextStyle(
-                color: Colors.white70,
+                color: Colors.black38,
                 fontSize: 16
               ),) ,
               ),
@@ -54,35 +54,35 @@ class Profile extends StatelessWidget {
           Padding(padding: EdgeInsets.only(top:270,left: 20),
               child: TextButton(onPressed: (){}, child: Text('Quizer',
               style: TextStyle(
-                color: Colors.white70,
+                color: Colors.black45,
                 fontWeight: FontWeight.bold,
               ),))
           ),
           Padding(padding: EdgeInsets.only(top:270,left: 100),
               child: TextButton(onPressed: (){}, child: Text('Plays',
               style: TextStyle(
-                color: Colors.white70,
+                color: Colors.black45,
                 fontWeight: FontWeight.bold,
               ),))
           ),
           Padding(padding: EdgeInsets.only(top:270,left: 180),
               child: TextButton(onPressed: (){}, child: Text('Players',
               style: TextStyle(
-                color: Colors.white70,
+                color: Colors.black45,
                 fontWeight: FontWeight.bold,
               ),))
           ),
           Padding(padding: EdgeInsets.only(top:270,left: 270),
               child: TextButton(onPressed: (){}, child: Text('Followers',
               style: TextStyle(
-                color: Colors.white70,
+                color: Colors.black45,
                 fontWeight: FontWeight.bold,
               ),))
           ),
           Padding(padding: EdgeInsets.only(top:310,left: 20),
               child: TextButton(onPressed: (){}, child: Text('30',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
                 fontSize: 16
               ),))
@@ -90,7 +90,7 @@ class Profile extends StatelessWidget {
           Padding(padding: EdgeInsets.only(top:310,left: 100),
               child: TextButton(onPressed: (){}, child: Text('90M',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
                 fontSize: 16
               ),))
@@ -98,7 +98,7 @@ class Profile extends StatelessWidget {
           Padding(padding: EdgeInsets.only(top:310,left: 185),
               child: TextButton(onPressed: (){}, child: Text('4B',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
                 fontSize: 16
               ),))
@@ -106,13 +106,14 @@ class Profile extends StatelessWidget {
           Padding(padding: EdgeInsets.only(top:310,left: 280),
               child: TextButton(onPressed: (){}, child: Text('390',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
                 fontSize: 16
               ),))
           ),
           Padding(padding: EdgeInsets.only(top:360,left: 10,right: 10),
-          child:TextButton(onPressed: (){}, child: Container(
+          child:TextButton(onPressed: (){},
+           child: Container(
             height: 50,
             width: 400,
             decoration: 
